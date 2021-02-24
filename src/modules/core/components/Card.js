a4bc6d7b391831ee15d48bc8b3d17d4432beb628
Pappa <dr.pappa@gmail.com>
2021-02-24 08:09:03 +0000

    Removing material-ui

---
+++ src/modules/core/components/Card.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  top: 4rem;
+  bottom: 4rem;
+  left: 4rem;
+  right: 4rem;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  border-radius: 0.5rem;
+  background-color: white;
+`;
--- src/modules/welcome/Welcome.js
+++ src/modules/welcome/Welcome.js
@@ -1,36 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Backdrop, Button } from "@material-ui/core";
 import { loginAsGuestAction } from "../core/actions";
-import styled from "styled-components";
-
-const WelcomeBackground = styled.div`
-  top: 4rem;
-  bottom: 4rem;
-  left: 4rem;
-  right: 4rem;
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-  border-radius: 0.5rem;
-  background-color: white;
-`;
+import { Backdrop } from "../core/components/Backdrop";
+import { Card } from "../core/components/Card";
 
 export function Welcome({ loginAsGuest, isLoggedIn }) {
   return (
     <Backdrop open={!isLoggedIn}>
-      <WelcomeBackground>
-        <Button
-          variant="outlined"
-          color="secondary"
-          onClick={() => loginAsGuest()}
-        >
-          Login as a guest.
-        </Button>
-      </WelcomeBackground>
+      <Card>
+        <button onClick={() => loginAsGuest()}>Login as a guest.</button>
+      </Card>
     </Backdrop>
   );
 }
