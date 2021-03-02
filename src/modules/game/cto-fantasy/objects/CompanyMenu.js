0c7f601cfc5e25a08a0b41b23b2a5d2d55745479
Pappa <dr.pappa@gmail.com>
2021-03-02 21:35:06 +0000

    Added selection event

--- src/modules/game/cto-fantasy/objects/CompanyMenu.js
+++ src/modules/game/cto-fantasy/objects/CompanyMenu.js
@@ -20,19 +20,26 @@ export class CompanyMenu extends Phaser.GameObjects.Container {
       .setOrigin(0.5, 0);
     const button = scene.add.image(0, 155, "button");
     const acceptText = scene.add
-      .text(0, 160, "Accept Job", {
+      .text(0, 160, "Accept Job Offer", {
         font: "12px Open Sans",
         fill: "#ffffff",
       })
       .setOrigin(0.5, 0);
 
     menu.setScale(1.5, 2).setOrigin(0.5, 0);
-    button.setScale(0.5).setOrigin(0.5, 0);
+    button.setScale(0.5).setOrigin(0.5, 0).setInteractive();
+
+    button.once("pointerup", this.selectCompany.bind(this, company), this);
 
     this.setSize(100, 150);
 
     this.add([menu, companyName, companyDescription, button, acceptText]);
 
     scene.add.existing(this);
+  }
+
+  selectCompany(company) {
+    // add company to global data and exit to MainScene
+    console.log(company);
   }
 }
