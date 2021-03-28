04474a3b4783f9a5be9bc4af518229700e9010e5
Pappa <dr.pappa@gmail.com>
2021-03-28 12:02:19 +0100

    Loading screen

--- code/cto-fantasy/src/modules/game/cto-fantasy/scenes/BootScene.js
+++ code/cto-fantasy/src/modules/game/cto-fantasy/scenes/BootScene.js
@@ -9,6 +9,100 @@ export class BootScene extends Phaser.Scene {
 
   // load assets
   preload() {
+    this.createGameTitle();
+    this.createProgressBar();
+    this.loadAssets();
+  }
+
+  createGameTitle() {
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var title = this.make.text({
+      x: width / 2,
+      y: height / 2 - 220,
+      text: "Project Genesis",
+      style: {
+        font: "24px monospace",
+        fill: "#ffffff",
+      },
+    });
+    title.setOrigin(0.5, 0.5);
+
+    var subtitle = this.make.text({
+      x: width / 2,
+      y: height / 2 - 180,
+      text: "A software development fantasy",
+      style: {
+        font: "18px monospace",
+        fill: "#ffffff",
+      },
+    });
+    subtitle.setOrigin(0.5, 0.5);
+  }
+
+  createProgressBar() {
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: "Loading...",
+      style: {
+        font: "20px monospace",
+        fill: "#ffffff",
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: "0%",
+      style: {
+        font: "18px monospace",
+        fill: "#ffffff",
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: "",
+      style: {
+        font: "18px monospace",
+        fill: "#ffffff",
+      },
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on("progress", function (value) {
+      percentText.setText(parseInt(value * 100) + "%");
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on("fileprogress", function (file) {
+      assetText.setText("Loading asset: " + file.key);
+    });
+
+    this.load.on("complete", function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+  }
+
+  loadAssets() {
     this.load.html("joinform", "assets/forms/join.html");
     this.load.html("card", "assets/forms/card.html");
     this.load.html("event", "assets/forms/event.html");
