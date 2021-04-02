efc32e79dbd1bf0b4b5556a5ce2e6be9dd66ac68
Pappa <dr.pappa@gmail.com>
2021-04-02 11:18:56 +0100

    Added project backlog

--- code/cto-fantasy/src/classes/Project.js
+++ code/cto-fantasy/src/classes/Project.js
@@ -1,6 +1,7 @@
 export class Project {
   constructor({ name } = {}) {
     this.projectName = name || "Project Genesis";
+    this.backlog = [];
     this.numberOfBugs = 0;
     this.testCoverage = 0;
   }
