5d0243e94a2e175685594cd1e1ebd138e2594e97
Pappa <dr.pappa@gmail.com>
2021-06-07 17:25:00 +0100

    team scene updated

--- code/cto-fantasy/src/scenes/TeamScene.js
+++ code/cto-fantasy/src/scenes/TeamScene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import { Card } from "../game-objects/Card";
+import { SceneBackground } from "../game-objects/SceneBackground";
 import * as theme from "../theme";
 
 export class TeamScene extends Phaser.Scene {
@@ -20,38 +21,37 @@ export class TeamScene extends Phaser.Scene {
   }
 
   createComponents() {
-    this.header = this.add
-      .text(400, 15, " ", {
-        ...theme.h1,
-        align: "center",
-        wordWrap: { width: 500, useAdvancedWrap: true },
-      })
-      .setOrigin(0.5, 0);
-    this.close = this.add
-      .text(760, 15, "X", theme.x)
-      .setOrigin(0)
-      .setInteractive({ useHandCursor: true })
-      .on("pointerup", () => {
-        this.onClose();
-      });
-  }
-
-  meetTheTeam() {
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
     const name = this.registry.get("name");
     const company = this.registry.get("company");
-    const headerText = this.intro
+    const title = this.intro
       ? `Welcome to ${company.name} ${name}!\nCome and meet the team.`
       : `The ${company.name} team.`;
 
     this.intro = false;
-    this.header.setText(headerText);
+    this.background = new SceneBackground(this, 0, 0, width, height, {
+      title,
+      closeIcon: "complete_icon",
+      onClose: () => {
+        this.onClose();
+      },
+    });
+  }
 
+  meetTheTeam() {
     this.teamCards = this.team.members.map((member, idx) => {
       const x = -50 + (idx + 1) * 175;
+      const ratings = Array(5).fill("\u2606");
+      const memberRatings = Array(member.rating).fill("\u2605");
+      ratings.splice(0, member.rating, ...memberRatings);
+      const ratingText = ratings.join(" ");
       return this.add.existing(
         new Card(this, x, 150, {
           title: member.name,
-          text: member.type,
+          text: `${member.type}<br/><br/>${
+            member.experience
+          } years exp.<br/>Salary: £${member.salary.toLocaleString()}<br/><br/>${ratingText}`,
         })
       );
     }, this);
